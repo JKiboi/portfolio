8c248947d457497d12ce942c7b0840e6f9639c69
JKiboi <anythingtechke@gmail.com>
2024-03-21 17:18:55 +0300

    new commit

--- src/resources/ProjectList.js
+++ src/resources/ProjectList.js
@@ -14,11 +14,29 @@ import waterapp from "../assets/annelle-home.png";
 import reduxapp from "../assets/reduxapp.png";
 import event from "../assets/event.png";
 import lowest from "../assets/lowest.png";
+import hosi from "../assets/hosi.png";
+import billy from "../assets/billy.png"
 
 export const ProjectList = [
   
   {
     id: 1,
+    image: hosi,
+    link: "https://hosi-app.onrender.com",
+    name: "Hospital management app",
+    skills: "ReactJS,Node.js,Express.js,MongoDB Atlas",
+    github: "",
+  },
+  {
+    id: 2,
+    image: billy,
+    link: "https://mern-tours-app.onrender.com",
+    name: "Billy Travels app",
+    skills: "ReactJS,Node.js,Express.js,MongoDB Atlas,Daraja API",
+    github: "",
+  },
+  {
+    id: 3,
     image: lowest,
     link: "https://lowest-unique-bidder-app.onrender.com",
     name: "Lowest unique bidding app",
@@ -26,25 +44,23 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 2,
+    id: 4,
     image: waterapp,
     link: "https://mern-annelle-app.onrender.com",
     name: "Business management app",
     skills: "ReactJS,Node.js,Express.js,MongoDB Atlas",
     github: "https://github.com/JKiboi/mern-annelle-waters-app-frontend",
   },
-
   {
-    id: 3,
+    id: 5,
     image: bidmaniaapp,
     link: "https://angukia-mali3-app.onrender.com",
     name: "Online bidding app",
     skills: "ReactJS,Node.js,Express.js,MongoDB Atlas,mpesa-Daraja API",
     github: "",
   },
-
   {
-    id: 4,
+    id: 6,
     image: commerce2,
     link: "https://singular-piroshki-00fdce.netlify.app/",
     name: "E commerce App",
@@ -52,111 +68,99 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 5,
+    id: 7,
     image: event,
     link: "https://event-tickets-app.onrender.com",
     name: "Event Ticketing App",
     skills: "ReactJS,Node.js,Express.js,MongoDB Atlas,mpesa-Daraja API",
     github: "",
   },
-
   {
-    id: 6,
+    id: 8,
     image: travels,
     link: "https://tours-and-travel-app.onrender.com",
     name: "Travel and Tours",
     skills: "ReactJS,Node.js,Express.js,MongoDB Atlas",
     github: "",
   },
-
   {
-    id: 7,
+    id: 9,
     image: portfolio,
     link: "https://whimsical-blini-647ba1.netlify.app/",
     name: "Portfolio",
     skills: "ReactJS ,CSS ,HTML",
     github: "",
   },
-
   {
-    id: 8,
+    id: 10,
     image: weather,
     link: "https://harmonious-sfogliatella-7ad5e5.netlify.app/",
     name: "Weather App",
     skills: "ReactJS ,CSS ,HTML",
     github: "https://github.com/JKiboi/weather-app-two.git",
   },
-
   {
-    id: 9,
+    id: 11,
     image: quiz,
     link: "https://tv-quiz-kenya.netlify.app/",
     name: "Quiz App",
     skills: "ReactJS ,CSS ,HTML",
     github: "https://github.com/JKiboi/quiz-app-two.git",
   },
-
   {
-    id: 10,
+    id: 12,
     image: calculator,
     link: "https://boisterous-lokum-6251ea.netlify.app/",
     name: "Calculator App",
     skills: "ReactJS ,CSS ,HTML",
     github: "https://github.com/JKiboi/react-calculator-two.git",
   },
-
   {
-    id: 11,
+    id: 13,
     image: movie,
     link: "https://extraordinary-pithivier-10754f.netlify.app/",
     name: "Movie App",
     skills: "JReactJS ,CSS ,HTML",
     github: "",
   },
-
   {
-    id: 12,
+    id: 14,
     image: recipe,
     link: "https://elegant-bombolone-d7cb31.netlify.app/",
     name: "Recipe App",
     skills: "ReactJS ,CSS ,HTML",
     github: "",
   },
-
   {
-    id: 13,
+    id: 15,
     image: reduxapp,
     link: "https://redux-counter-todo-app.onrender.com/",
     name: "Redux counter todos app",
     skills: "ReactJS,redux ,CSS ,HTML",
     github: "",
   },
-
   {
-    id: 13,
+    id: 16,
     image: radio,
     link: "https://boisterous-phoenix-e5962c.netlify.app/",
     name: "Radio lottery app",
     skills: "ReactJS ,CSS ,HTML",
     github: "",
   },
-
   {
-    id: 14,
+    id: 17,
     image: random,
     link: "https://singular-sprite-dd88fb.netlify.app/",
     name: "Random quote bot",
     skills: "ReactJS ,CSS ,HTML",
     github: "https://github.com/JKiboi/random-quote-bot.git",
   },
-
   {
-    id: 15,
+    id: 18,
     image: recipe1,
     link: "https://celadon-croquembouche-2829f6.netlify.app",
     name: "Random Recipe Generator",
     skills: "ReactJS ,CSS ,HTML",
     github: "https://github.com/JKiboi/random-recipe-app.git",
   },
-
 ];
