53812c66dfa258fdad0defe062fe52e43c84b52a
JKiboi <anythingtechke@gmail.com>
2025-04-15 21:23:38 +0300

    new comit

--- src/resources/ProjectList.js
+++ src/resources/ProjectList.js
@@ -2,25 +2,23 @@ import weather from "../assets/weather.png";
 import calculator from "../assets/calculator.png";
 import quiz from "../assets/quiz.png";
 import recipe from "../assets/recipe.png";
-import movie from "../assets/movie.png";
-import recipe1 from "../assets/recipe1.png";
+import movies from "../assets/movies.png";
 import commerce2 from "../assets/commerce2.png";
-import random from "../assets/random.png";
 import portfolio from "../assets/portfolio.png";
 import travels from "../assets/travels.png";
-import radio from "../assets/radio.png";
 import bidmaniaapp from "../assets/bidmaniaapp.png";
 import waterapp from "../assets/annelle-home.png";
 import reduxapp from "../assets/reduxapp.png";
-import event from "../assets/event.png";
 import lowest from "../assets/lowest.png";
 import hosi from "../assets/hosi.png";
 import billy from "../assets/billy.png";
 import payment from "../assets/payment.png";
 import cliniclog from "../assets/cliniclog.png";
-import corpTaxi from "../assets/corpTaxi.png";
+import events from "../assets/events.png";
 import bikes from "../assets/bikes.png";
 import maths from "../assets/maths.png";
+import aviator from "../assets/aviator.png";
+import meals from "../assets/meals.png";
 
 export const ProjectList = [
   {
@@ -33,6 +31,38 @@ export const ProjectList = [
   },
   {
     id: 2,
+    image: events,
+    link: "https://new-event-ticketing-app.onrender.com",
+    name: "Event Ticketing app",
+    skills: "ReactJS, Node.js, Express.js, MongoDB Atlas,RestfulApi,Daraja API",
+    github: "",
+  },
+  {
+    id: 3,
+    image: meals,
+    link: "https://hotel-meals-booking-app.onrender.com",
+    name: "Hotel Meals Booking app",
+    skills: "ReactJS, Node.js, Express.js, MongoDB Atlas",
+    github: "",
+  },
+  {
+    id: 4,
+    image: aviator,
+    link: "https://aviatorsim-app.onrender.com",
+    name: "Simulated Aviator game app",
+    skills: "ReactJS, Node.js, Express.js, MongoDB Atlas",
+    github: "",
+  },
+  {
+    id: 5,
+    image: movies,
+    link: "https://movies-app-updated.onrender.com",
+    name: "Movie Review App",
+    skills: "ReactJS, CSS, HTML",
+    github: "",
+  },
+  {
+    id: 6,
     image: maths,
     link: "https://interactive-maths-lab.onrender.com",
     name: "Interactive-Maths app",
@@ -40,15 +70,15 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 3,
-    image: corpTaxi,
-    link: "https://corporate-taxi-app.onrender.com",
-    name: "Corporate Taxi app",
-    skills: "ReactJS, Node.js, Express.js, MongoDB Atlas",
-    github: "",
+    id: 7,
+    image: quiz,
+    link: "https://tv-quiz-app.onrender.com",
+    name: "Quiz App",
+    skills: "ReactJS, CSS, HTML",
+    github: "https://github.com/JKiboi/quiz-app-two.git",
   },
   {
-    id: 4,
+    id: 8,
     image: hosi,
     link: "https://saas-hosi-man-app.onrender.com",
     name: "Hospital management app",
@@ -56,7 +86,7 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 5,
+    id: 9,
     image: billy,
     link: "https://mern-tours-app.onrender.com",
     name: "Billy Travels app",
@@ -64,7 +94,7 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 6,
+    id: 10,
     image: lowest,
     link: "https://lowest-unique-bidder-app.onrender.com",
     name: "Lowest unique bidding app",
@@ -72,7 +102,7 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 7,
+    id: 11,
     image: cliniclog,
     link: "https://cliniclog-app.onrender.com",
     name: "Clinic Log",
@@ -80,7 +110,7 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 8,
+    id: 12,
     image: payment,
     link: "https://hosi-payments-app.onrender.com",
     name: "Payment Tracking App",
@@ -88,7 +118,7 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 9,
+    id: 13,
     image: waterapp,
     link: "https://mern-annelle-app.onrender.com",
     name: "Business management app",
@@ -96,7 +126,7 @@ export const ProjectList = [
     github: "https://github.com/JKiboi/mern-annelle-waters-app-frontend",
   },
   {
-    id: 10,
+    id: 14,
     image: bidmaniaapp,
     link: "https://angukia-mali3-app.onrender.com",
     name: "Online bidding app",
@@ -104,7 +134,7 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 11,
+    id: 15,
     image: commerce2,
     link: "https://singular-piroshki-00fdce.netlify.app/",
     name: "E commerce App",
@@ -112,15 +142,7 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 12,
-    image: event,
-    link: "https://event-tickets-app.onrender.com",
-    name: "Event Ticketing App",
-    skills: "ReactJS, Node.js, Express.js, MongoDB Atlas, mpesa-Daraja API",
-    github: "",
-  },
-  {
-    id: 13,
+    id: 16,
     image: travels,
     link: "https://tours-and-travel-app.onrender.com",
     name: "Travel and Tours",
@@ -128,7 +150,7 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 14,
+    id: 17,
     image: portfolio,
     link: "https://whimsical-blini-647ba1.netlify.app/",
     name: "Portfolio",
@@ -136,7 +158,7 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 15,
+    id: 18,
     image: weather,
     link: "https://harmonious-sfogliatella-7ad5e5.netlify.app/",
     name: "Weather App",
@@ -144,15 +166,7 @@ export const ProjectList = [
     github: "https://github.com/JKiboi/weather-app-two.git",
   },
   {
-    id: 16,
-    image: quiz,
-    link: "https://tv-quiz-kenya.netlify.app/",
-    name: "Quiz App",
-    skills: "ReactJS, CSS, HTML",
-    github: "https://github.com/JKiboi/quiz-app-two.git",
-  },
-  {
-    id: 17,
+    id: 19,
     image: calculator,
     link: "https://boisterous-lokum-6251ea.netlify.app/",
     name: "Calculator App",
@@ -160,15 +174,7 @@ export const ProjectList = [
     github: "https://github.com/JKiboi/react-calculator-two.git",
   },
   {
-    id: 18,
-    image: movie,
-    link: "https://movies-app-updated.onrender.com",
-    name: "Movie App",
-    skills: "ReactJS, CSS, HTML",
-    github: "",
-  },
-  {
-    id: 19,
+    id: 20,
     image: recipe,
     link: "https://elegant-bombolone-d7cb31.netlify.app/",
     name: "Recipe App",
@@ -176,35 +182,11 @@ export const ProjectList = [
     github: "",
   },
   {
-    id: 20,
+    id: 21,
     image: reduxapp,
     link: "https://redux-counter-todo-app.onrender.com/",
     name: "Redux counter todos app",
     skills: "ReactJS, redux, CSS, HTML",
     github: "",
   },
-  {
-    id: 21,
-    image: radio,
-    link: "https://boisterous-phoenix-e5962c.netlify.app/",
-    name: "Radio lottery app",
-    skills: "ReactJS, CSS, HTML",
-    github: "",
-  },
-  {
-    id: 22,
-    image: random,
-    link: "https://singular-sprite-dd88fb.netlify.app/",
-    name: "Random quote bot",
-    skills: "ReactJS, CSS, HTML",
-    github: "https://github.com/JKiboi/random-quote-bot.git",
-  },
-  {
-    id: 23,
-    image: recipe1,
-    link: "https://celadon-croquembouche-2829f6.netlify.app",
-    name: "Random Recipe Generator",
-    skills: "ReactJS, CSS, HTML",
-    github: "https://github.com/JKiboi/random-recipe-app.git",
-  },
-];
+]
