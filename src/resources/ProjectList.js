554d91b81e1514e063563036fc006312257ae33f
JKiboi <anythingtechke@gmail.com>
2025-08-05 18:22:54 +0300

    new commit

--- src/resources/ProjectList.js
+++ src/resources/ProjectList.js
@@ -1,28 +1,38 @@
-import weather from "../assets/weather.png";
-import calculator from "../assets/calculator.png";
-import quiz from "../assets/quiz.png";
-import recipe from "../assets/recipe.png";
-import movies from "../assets/movies.png";
-import commerce2 from "../assets/commerce2.png";
-import portfolio from "../assets/portfolio.png";
-import travels from "../assets/travels.png";
-import bidmaniaapp from "../assets/bidmaniaapp.png";
-import waterapp from "../assets/annelle-home.png";
-import reduxapp from "../assets/reduxapp.png";
-import lowest from "../assets/lowest.png";
-import hosi from "../assets/hosi.png";
-import billy from "../assets/billy.png";
-import payment from "../assets/payment.png";
-import cliniclog from "../assets/cliniclog.png";
+import hrsystem from "../assets/hrsystem.png";
+import hvacflow from "../assets/hvac-flow2.png";
 import events from "../assets/events1.png";
 import bikes from "../assets/bikes.png";
 import maths from "../assets/maths.png";
 import aviator from "../assets/aviator.png";
 import meals from "../assets/meals.png";
+import movies from "../assets/movies.png";
+import quiz from "../assets/quiz.png";
+import hosi from "../assets/hosi.png";
+import billy from "../assets/billy.png";
+import lowest from "../assets/lowest.png";
+import cliniclog from "../assets/cliniclog.png";
 
 export const ProjectList = [
   {
     id: 1,
+    image: hrsystem,
+    link: "https://hrsystemke.onrender.com",
+    name: "HR System",
+    skills: "ReactJS, Node.js, Express.js, MongoDB Atlas",
+    github: "",
+    category: "Full-Stack",
+  },
+  {
+    id: 2,
+    image: hvacflow,
+    link: "https://hvac-flow.onrender.com",
+    name: "HVAC Flow",
+    skills: "ReactJS, Node.js, Express.js, MongoDB Atlas",
+    github: "",
+    category: "Full-Stack",
+  },
+  {
+    id: 3,
     image: bikes,
     link: "https://motorbike-credit-sales-app.onrender.com",
     name: "Motorbike Credit Sales app",
@@ -31,7 +41,7 @@ export const ProjectList = [
     category: "Full-Stack",
   },
   {
-    id: 2,
+    id: 4,
     image: events,
     link: "https://tikitiflow.onrender.com",
     name: "Event Ticketing app",
@@ -40,7 +50,7 @@ export const ProjectList = [
     category: "Full-Stack",
   },
   {
-    id: 3,
+    id: 5,
     image: meals,
     link: "https://hotel-meals-booking-app.onrender.com",
     name: "Hotel Meals Booking app",
@@ -49,7 +59,7 @@ export const ProjectList = [
     category: "Full-Stack",
   },
   {
-    id: 4,
+    id: 6,
     image: aviator,
     link: "https://aviatorsim-app.onrender.com",
     name: "Simulated Aviator game app",
@@ -58,7 +68,7 @@ export const ProjectList = [
     category: "Full-Stack",
   },
   {
-    id: 5,
+    id: 7,
     image: movies,
     link: "https://movies-app-updated.onrender.com",
     name: "Movie Review App",
@@ -67,7 +77,7 @@ export const ProjectList = [
     category: "Front-End",
   },
   {
-    id: 6,
+    id: 8,
     image: maths,
     link: "https://interactive-maths-lab.onrender.com",
     name: "Interactive-Maths app",
@@ -76,7 +86,7 @@ export const ProjectList = [
     category: "Front-End",
   },
   {
-    id: 7,
+    id: 9,
     image: quiz,
     link: "https://tv-quiz-app.onrender.com",
     name: "Quiz App",
@@ -85,7 +95,7 @@ export const ProjectList = [
     category: "Front-End",
   },
   {
-    id: 8,
+    id: 10,
     image: hosi,
     link: "https://saas-hosi-man-app.onrender.com",
     name: "Hospital management app",
@@ -94,7 +104,7 @@ export const ProjectList = [
     category: "Full-Stack",
   },
   {
-    id: 9,
+    id: 11,
     image: billy,
     link: "https://mern-tours-app.onrender.com",
     name: "Billy Travels app",
@@ -103,7 +113,7 @@ export const ProjectList = [
     category: "Full-Stack",
   },
   {
-    id: 10,
+    id: 12,
     image: lowest,
     link: "https://lowest-unique-bidder-app.onrender.com",
     name: "Lowest unique bidding app",
@@ -112,102 +122,12 @@ export const ProjectList = [
     category: "Full-Stack",
   },
   {
-    id: 11,
+    id: 13,
     image: cliniclog,
     link: "https://cliniclog-app.onrender.com",
     name: "Clinic Log",
     skills: "ReactJS, Node.js, Express.js, MongoDB Atlas",
     github: "",
     category: "Full-Stack",
-  },
-  {
-    id: 12,
-    image: payment,
-    link: "https://hosi-payments-app.onrender.com",
-    name: "Payment Tracking App",
-    skills: "ReactJS, Node.js, Express.js, exceljs",
-    github: "",
-    category: "Full-Stack",
-  },
-  {
-    id: 13,
-    image: waterapp,
-    link: "https://mern-annelle-app.onrender.com",
-    name: "Business management app",
-    skills: "ReactJS, Node.js, Express.js, MongoDB Atlas",
-    github: "https://github.com/JKiboi/mern-annelle-waters-app-frontend",
-    category: "Full-Stack",
-  },
-  {
-    id: 14,
-    image: bidmaniaapp,
-    link: "https://angukia-mali3-app.onrender.com",
-    name: "Online bidding app",
-    skills: "ReactJS, Node.js, Express.js, MongoDB Atlas, mpesa-Daraja API",
-    github: "",
-    category: "Full-Stack",
-  },
-  {
-    id: 15,
-    image: commerce2,
-    link: "https://singular-piroshki-00fdce.netlify.app/",
-    name: "E commerce App",
-    skills: "ReactJS, CSS, HTML",
-    github: "",
-    category: "Front-End",
-  },
-  {
-    id: 16,
-    image: travels,
-    link: "https://tours-and-travel-app.onrender.com",
-    name: "Travel and Tours",
-    skills: "ReactJS, Node.js, Express.js, MongoDB Atlas",
-    github: "",
-    category: "Full-Stack",
-  },
-  {
-    id: 17,
-    image: portfolio,
-    link: "https://whimsical-blini-647ba1.netlify.app/",
-    name: "Portfolio",
-    skills: "ReactJS, CSS, HTML",
-    github: "",
-    category: "Front-End",
-  },
-  {
-    id: 18,
-    image: weather,
-    link: "https://harmonious-sfogliatella-7ad5e5.netlify.app/",
-    name: "Weather App",
-    skills: "ReactJS, CSS, HTML",
-    github: "https://github.com/JKiboi/weather-app-two.git",
-    category: "Front-End",
-  },
-  {
-    id: 19,
-    image: calculator,
-    link: "https://boisterous-lokum-6251ea.netlify.app/",
-    name: "Calculator App",
-    skills: "ReactJS, CSS, HTML",
-    github: "https://github.com/JKiboi/react-calculator-two.git",
-    category: "Front-End",
-  },
-  {
-    id: 20,
-    image: recipe,
-    link: "https://elegant-bombolone-d7cb31.netlify.app/",
-    name: "Recipe App",
-    skills: "ReactJS, CSS, HTML",
-    github: "",
-    category: "Front-End",
-  },
-  {
-    id: 21,
-    image: reduxapp,
-    link: "https://redux-counter-todo-app.onrender.com/",
-    name: "Redux counter todos app",
-    skills: "ReactJS, redux, CSS, HTML",
-    github: "",
-    category: "Front-End",
   },
 ]
